0dd6b03564d1e4cf16d6989366f4eec93bddf82c
HieuNotGood <pdhhieu27032004@gmail.com>
2025-03-05 19:08:35 +0700

    Cap nhat lab 2

---
+++ ASC.Model/Models/MasterDataValue.cs
@@ -0,0 +1,22 @@
+﻿using ASC.Model.BaseTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASC.Model.Models
+{
+    public class MasterDataValue : BaseEntity, IAuditTracker
+    {
+        public MasterDataValue() { }
+        public MasterDataValue(string masterDataPartitionkey, string value)
+        {
+            this.PartitionKey = masterDataPartitionkey;
+            this.Rowkey = Guid.NewGuid().ToString();
+        }
+
+        public bool IsActive { get; set; }
+        public string Name { get; set; }
+    }
+}
--- ASC.WEB/Data/ApplicationDbContext.cs
+++ ASC.WEB/Data/ApplicationDbContext.cs
@@ -1,13 +1,30 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using ASC.Model.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ASC.WEB.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public virtual DbSet<MasterDataKey> MasterDataKeys { get; set; }
+        public virtual DbSet<MasterDataValue> MasterDataValues { get; set; }
+        public virtual DbSet<ServiceRequest> ServiceRequests { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
+            Database.Migrate();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<MasterDataKey>()
+                .HasKey(c => new { c.PartitionKey, c.Rowkey });
+            builder.Entity<MasterDataValue>()
+                .HasKey(c => new { c.PartitionKey, c.Rowkey });
+            builder.Entity<ServiceRequest>()
+                .HasKey(c => new { c.PartitionKey, c.Rowkey });
+
+            base.OnModelCreating(builder);
         }
     }
 }
